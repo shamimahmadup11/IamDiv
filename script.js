2848e4d8bcfc2f28f32c4750e5586231a2abd3d1
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-02-28 18:47:15 +0530

    changing style by javaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+const mydiv=document.querySelector(".container");
+
+// console.log(mydiv);
+mydiv.style.background="yellow";
+mydiv.style.color="blue";
+mydiv.style.height="340px";
+mydiv.style.width="340px";
+mydiv.style.margin="60px";
+mydiv.style.padding="30px";
+mydiv.style.fontSize="30px";
+mydiv.style.fontWeight="30px";
+
+
